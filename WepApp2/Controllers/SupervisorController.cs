abacfd0e2acfb8707ee76b0fcf5bfbc2659455ce
Leena-alnahdi <lalnahdi0008@stu.kau.edu.sa>
2025-06-27 23:21:02 +0300

    Add project files.

---
+++ WepApp2/Controllers/SupervisorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteProject.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
