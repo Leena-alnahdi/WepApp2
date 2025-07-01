71161451f69b69d79cc682b97466a6646477ac0c
Asma Saud Omar <asmasauod3@gmail.com>
2025-07-01 12:02:05 +0300

    login page

---
+++ WepApp2/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace KauAuthPortal.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
